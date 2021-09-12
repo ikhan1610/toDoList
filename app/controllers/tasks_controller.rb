f790e9ca2d72ee2a46d18f24ee8c0c7e44a544e1
Khan Immran <khan.immran@gmail.com>
2021-09-12 06:54:56 +0530

    Made changes to root route and accordingly reviewd and rivsed sessions/new and tasks/edit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,7 +5,7 @@ class TasksController < ApplicationController
 
   # GET /tasks or /tasks.json
   def index
-    @tasks = current_user.tasks
+    @tasks = Task.all
   end
 
   # GET /tasks/1 or /tasks/1.json
